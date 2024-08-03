9dd3440fdefae1046e6461ac7b2b5acde222a112
ParthTrivedi122 <124391924+ParthTrivedi122@users.noreply.github.com>
2024-08-03 20:54:45 +0530

    vercel.json added

--- KJSCE.session.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO enrollments VALUES (135,'parth.lt@somaiya.edu','6MvmTJFlEeauIxKYfUIOvQ',10,0,1,'online','OET','VI','2023-2024',0)
-,(136,'parth.lt@somaiya.edu','dQMNsk2NEeW2EwoziUhyxQ',10,0,1,'online','OET','VI','2023-2024',0),
-(137,'parth.lt@somaiya.edu','VlLC102NEeWEHRLwpfV5DQ',10,0,1,'online','OET','VI','2023-2024',0),
-(138,'parth.lt@somaiya.edu','XDJWC7ZqEeaa7BLnmaLUtg',10,0,1,'online','OET','VI','2023-2024',0)
