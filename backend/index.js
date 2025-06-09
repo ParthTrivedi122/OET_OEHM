053415342f9ab12043ffb6327cd056036bdcbda3
ParthTrivedi122 <124391924+ParthTrivedi122@users.noreply.github.com>
2025-06-09 13:18:36 +0530

    final changes

--- backend/index.js
+++ backend/index.js
@@ -58,7 +58,7 @@ const con = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port:process.env.DB_port,
+  port:process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
